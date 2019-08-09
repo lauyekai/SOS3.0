64fe6ccf3e2f4c5328eb2ba4f71652ff150e1a0d
lauyekai <lauyk-wa15@student.tarc.edu.my>
2019-08-09 23:26:32 +0800

    commit

--- app/src/main/java/com/example/sos/contacts.java
+++ app/src/main/java/com/example/sos/contacts.java
@@ -1,29 +1,46 @@
 package com.example.sos;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.example.sos.modal.Contact;
+import com.example.sos.sql.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class contacts extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
+    private AppCompatActivity activity = contacts.this;
+    private RecyclerView recyclerViewContact;
+    private List<Contact> listContact;
+    private ContactAdapter contactAdapter;
+    private DatabaseHelper databaseHelper;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
+        initViews();
+        initObjects();
+
         BottomNavigationView topView = findViewById(R.id.nav_view_top) ;
         topView.setOnNavigationItemSelectedListener(this);
-
-          topView.getMenu() .getItem(1).setChecked(true);
-
-
-
+        topView.getMenu() .getItem(1).setChecked(true);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,24 +49,48 @@ public class contacts extends AppCompatActivity implements BottomNavigationView.
                 Intent intent = new Intent(contacts.this,add_contact.class);
                 startActivity(intent);
 
-
             }
         });
 
     }
 
-
-    private boolean loadFragemt(Fragment fragment)
-    {
-        if(fragment!=null)
-        {
-            getSupportFragmentManager() .beginTransaction() .replace(R.id.frame_container,fragment).commit() ;
-            return true;
-        }
-        return false;
-
-
+    private void initViews() {
+        recyclerViewContact = (RecyclerView) findViewById(R.id.recyclerViewContact);
     }
+
+    private void initObjects() {
+        listContact = new ArrayList<>();
+        contactAdapter = new ContactAdapter(listContact);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewContact.setLayoutManager(mLayoutManager);
+        recyclerViewContact.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewContact.setAdapter(contactAdapter);
+        databaseHelper = new DatabaseHelper(activity);
+
+
+        getDataFromSQLite();
+    }
+
+    private void getDataFromSQLite() {
+        // AsyncTask is used that SQLite operation not blocks the UI Thread.
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                listContact.clear();
+                listContact.addAll(databaseHelper.getAllContact());
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                contactAdapter.notifyDataSetChanged();
+            }
+        }.execute();
+    }
+
+
     public void openMainActivity()
     {  Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
@@ -58,17 +99,21 @@ public class contacts extends AppCompatActivity implements BottomNavigationView.
     {  Intent intent = new Intent(this,Setting.class);
         startActivity(intent);
     }
+    public void openRescueFragment()
+    {  Intent intent = new Intent(this,rescuefragment.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        Fragment fragment =null;
+
         switch (menuItem.getItemId())
         {
             case R.id.navigation_home :
                 openMainActivity();
                 break;
             case R.id.navigation_rescue:
-                fragment = new rescuefragment() ;
+                openRescueFragment();
                 break;
             case R.id.navigation_contacts:
                 break;
@@ -77,7 +122,7 @@ public class contacts extends AppCompatActivity implements BottomNavigationView.
                 break;
 
         }
-        return loadFragemt(fragment);
+        return true;
     }
 
 
